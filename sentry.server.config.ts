629103f3d02fcf6ec66f30c249eeec4c70786577
oweaj <qwe1520156@naver.com>
2024-11-21 09:51:11 +0900

    sentry dns env setting

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://6fc6a25368321db028d80cac8c72e864@o4508329920233472.ingest.de.sentry.io/4508329943367760",
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
